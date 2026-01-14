0542c7466520f4cdedfa0c1ac149d0bccda7dbaf
nekogakure <156896971+nekogakure@users.noreply.github.com>
2026-01-14 20:13:21 +0900

    add `cmate clean`

---
+++ src/clean/clean.rs
@@ -0,0 +1,19 @@
+use crate::{err, info};
+
+/// make cleanしてプロジェクトをクリーンアップ
+pub fn clean_project(make: String) {
+    println!("Cleaning project...");
+    let status = std::process::Command::new(make)
+        .arg("clean")
+        .status()
+        .unwrap_or_else(|e| {
+            err!("Failed to clean project: {}", e);
+            std::process::exit(1);
+        });
+
+    if !status.success() {
+        err!("make clean failed with status: {}", status);
+    } else {
+        info!("Project cleaned successfully.");
+    }
+}
